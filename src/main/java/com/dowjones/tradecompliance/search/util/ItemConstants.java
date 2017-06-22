3816c3ba03cd80ebe12381e42121b6d629a23526
Swathi <ramashes@act.org>
2017-06-22 15:38:31 +0530

    Corrected the spelling for Item in failure message

--- src/main/java/com/dowjones/tradecompliance/search/util/ItemConstants.java
+++ src/main/java/com/dowjones/tradecompliance/search/util/ItemConstants.java
@@ -10,7 +10,7 @@ public class ItemConstants {
 	
 	public static final String ITEM_CREATION_SUCCESS = "Item created successfully.";
 	public static final String SUCCESS = "SUCCESS";
-	public static final String ITEM_CREATION_FAILURE = "Iteam creation failed.";
+	public static final String ITEM_CREATION_FAILURE = "Item creation failed.";
 	public static final String FAILURE = "FAILURE";
 	public static final String ITEM_CREATION_ERROR = "Error while creating Item.";
 	public static final int ERRORCODE = 000;
